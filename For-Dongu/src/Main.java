383bd0594b4d5109ecb06087801ccca0dd8b6a86
efsa <113553752+efsacode@users.noreply.github.com>
2022-09-17 23:02:42 +0300

    For Loop

---
+++ For-Dongu/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] urun = {"Domates", "Patates", "Sogan", "Patlican", "Salatalik"};
+		
+		for (int i = 0; i<5; i++) {
+			System.out.println("Marketten alinacak " +(i+1) + ". urun = " +urun[i] );
+		}
+
+}
+}
